8e917dd41217350e37f517bedd5e4ba7bda020bc
vpreston <victorialynnpreston@gmail.com>
2014-11-20 12:36:14 -0500

    Updating zbar scripts

--- Vision/zbar_example.py
+++ Vision/zbar_example.py
@@ -1,4 +1,7 @@
 import zbar
+import numpy as np
+import cv2
+
  
 class QRCode():
  
@@ -19,21 +22,47 @@ class QRCode():
  
         self.proc.init("/dev/video0")
         self.proc.set_data_handler(self.qr_handler)
-        self.proc.visible = True
+        #self.proc.visible = True
 #display cam window if True,  hide if False
-        self.proc.active = True
+        #self.proc.active = True
+        self.quitter = 1
  
     def get_data(self):
-        self.proc.process_one()
-        print self.data
+        while (1):
+            try:
+                self.proc.process_one()
+                #self.quitter = input("1 to keep going:   ")
+                print self.data
+            except KeyboardInterrupt:
+                break
         return(self.data)
+
+    def image_display(self):
+        cap = cv2.VideoCapture(0)
+        while(True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+
+            # Our operations on the frame come here
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # Display the resulting frame
+            cv2.imshow('frame',gray)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # When everything done, release the capture
+        cap.release()
+        cv2.destroyAllWindows()
  
 #if(QRCode().get_data() == "test"):
 #	print "DATA IS test"
  
-data = QRCode().get_data()
-if (data == "test"):
-	print "data is test"
+# data = QRCode().get_data()
+# if (data == "test"):
+# 	print "data is test"
  
-#if(__name__ == "__main__"):
- #  print QRCode().get_data()
+if __name__ == '__main__':
+    data = QRCode().get_data()
+
+
